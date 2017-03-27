5b27252e328e4ce72f73e2f307ccee781b44896e
n3cs1s <pavel.primavera@gmail.com>
2017-03-27 14:31:22 +0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,10 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 echo "<!DOCTYPE html><html><head><title>Braille Ukraine - ресурсний центр для незрячих</title>";
+
 
 ?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
-<link rel="stylesheet" text="text/css" href="css/styles.css" />
+<link rel="stylesheet" text="text/css" href="/css/styles.css" />
 <?php
 echo '</head><body><h1>Вітаємо на ресурсному центрі для незрячих - Braille Ukraine<h1><br /></body>';
