02efc11f03628a1fa87c828c7f89eb1d6f428bb0
n3cs1s <pavel.primavera@gmail.com>
2017-03-27 14:15:59 +0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,6 @@
 <?php
 echo "<!DOCTYPE html><html><head><title>Braille Ukraine</title></head>";
 echo '<body>Hello, world!</body>';
+?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+<?php
