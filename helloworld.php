3c94b83f84cea0b8141e8d60e90268174e608c65
n3cs1s <pavel.primavera@gmail.com>
2017-03-27 15:02:27 +0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -6,5 +6,25 @@ echo "<!DOCTYPE html><html><head><title>Braille Ukraine - ресурсний ц�
 ?>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
 <link rel="stylesheet" text="text/css" href="https://cdn.rawgit.com/n3cs1s/braille-ua/9fac3c41/css/styles.css" />
+<link rel="stylesheet" text="text/css" href="https://rawgit.com/n3cs1s/braille-ua/master/css/styles.css" />
 <?php
-echo '</head><body><h1>Вітаємо на ресурсному центрі для незрячих - Braille Ukraine<h1><br /></body>';
+echo '</head><body><br />';
+?>
+<div id="header">
+  <div id="topbar">
+    <a href="#" title="Menu" onclick="$('#menu').toggle();"></a>
+    <div id="menu">
+      <ul>
+        <li><a href="#resources">Resources</a></li>
+        <li><a href="#music-braille">Music files</a></li>
+        <li><a href="#audio">Audio descriptions</a></li>
+      </ul>
+    </div>
+  </div>
+</div> <!--header -->
+<div id="main"><h1>Вітаємо на ресурсному центрі для незрячих - Braille Ukraine</h1></div>
+<div id="resources">Resources</div>
+<div id="music-braille">Music Braille</div>
+<div id="audio">Audio descriptions</div>
+<?php 
+echo '</body>';
