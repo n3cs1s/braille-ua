f60e50a72d4133fd2dd93a8d6734c1f761a79189
n3cs1s <pavel.primavera@gmail.com>
2017-03-27 16:36:29 +0300

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -12,22 +12,31 @@ echo '</head><body><br />';
 ?>
 <div id="header">
   <div id="topbar">
-    <a title="Menu" onclick="$('#menu').toggle();"><span class="span-bar"></span>
+    <a title="Меню" onclick="$('#menu').toggle();"><span class="span-bar"></span>
       <span class="span-bar"></span>
       <span class="span-bar"></span></a>
-    <a href="/" title="Main">Main</a>
+    <a href="/" title="Головна">Головна</a>
     <div id="menu">
       <ul>
-        <li><a href="#resources" onclick="$('#menu').hide();">Resources</a></li>
-        <li><a href="#music-braille" onclick="$('#menu').hide();">Music files</a></li>
-        <li><a href="#audio" onclick="$('#menu').hide();">Audio descriptions</a></li>
+        <li><a href="#resources" onclick="$('#menu').hide();">Ресурси</a></li>
+        <li><a href="#music-braille" onclick="$('#menu').hide();">Нотні видання</a></li>
+        <li><a href="#audio" onclick="$('#menu').hide();">Аудіокниги</a></li>
       </ul>
     </div>
   </div>
 </div> <!--header -->
 <div id="main"><h1>Вітаємо на ресурсному центрі для незрячих - Braille Ukraine</h1></div>
-<div id="resources">Resources</div>
-<div id="music-braille">Music Braille</div>
-<div id="audio">Audio descriptions</div>
+<div id="resources"><h2>Ресурси</h2></div>
+<div id="music-braille"><h2>Нотні видання</h2></div>
+<div id="audio"><h2>Аудіокниги</h2><br />
+  <ul id="list-audio">
+    <li>Книга 1</li>
+    <li>Книга 2</li>
+    <li>Книга 3</li>
+    <li>Книга 4</li>
+    <li>Книга 5</li>
+    <li>Книга 6</li>
+  </ul>
+</div>
 <?php 
 echo '</body>';
